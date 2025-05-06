b83471dd0f5c9cc779e89f715c081b863637494c
jkwoods <woodsjk@seas.upenn.edu>
2025-05-06 15:55:17 -0400

    todos

--- Lab14/src/cs101/ErrorPractice.java
+++ Lab14/src/cs101/ErrorPractice.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 
 public class ErrorPractice {
 
+	// 3 points
 	public static void a(int index) {
 
 		int[] a = { 1, 2, 3, 4 };
 
-		// put the following line of code inside of a try/catch block
+		// TODO: put the following line of code inside of a try/catch block
 		// the catch block should just print the exception
 
 		int element = a[index];
 
 	}
 
+	// 3 points
 	public static int b() {
 
-		// put the following two lines of code inside of a try/catch block
+		// TODO: put the following two lines of code inside of a try/catch block
 		// We expect a number from our user.
 		// the catch block should `return -1;`
 
@@ -27,11 +29,12 @@ public class ErrorPractice {
 
 	}
 
+	// 3 points
 	public static int c() {
 
 		int c = 0;
 
-		// add a finally block to this try/catch statement that adds 8 to c
+		// TODO: add a finally block to this try/catch statement that adds 8 to c
 		try {
 			int n = Integer.MAX_VALUE + 1;
 			c += n;
@@ -44,28 +47,31 @@ public class ErrorPractice {
 
 	}
 
+	// 3 points
 	public static void check_input(double input) {
 
-		// add an if statement here.
+		// TODO: add an if statement here.
 		// if input > 99.0, you should throw a `RuntimeException` error.
 		// the message can be whatever you want
 
 	}
 
+	// 3 points
 	public static void d() {
 
 		Scanner input = new Scanner(System.in); // Create a Scanner object
 
 		String n = input.nextLine();
 
-		// if n is the empty String "", you should throw an `IOException` error
+		// TODO: if n is the empty String "", you should throw an `IOException` error
 		// the message can be whatever you want
 
 	}
 
+	// 5 points
 	public static void e() {
 
-		// put the following line of code inside a try/catch block
+		// TODO: put the following line of code inside a try/catch block
 		// the catch block should catch the ArithmeticException type ONLY!
 		int x = divide(4, 0);
 
@@ -77,8 +83,10 @@ public class ErrorPractice {
 		return a / b;
 	}
 
+	// 5 points
 	public static int f(int p) {
-		// uncomment the following line and put it inside a try/catch block with two
+		// TODO: uncomment the following line and put it inside a try/catch block with
+		// two
 		// separate catch statements:
 		// - one for `RuntimeException`s that returns -1
 		// - one for `IOException`s that returns -2
